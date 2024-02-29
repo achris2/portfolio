411dae26f57a9520e0fab7666bc630b3459fd79b
Alex Christakou <alex@Alexs-Air-2.broadband>
2024-02-29 21:58:51 +0000

    fixed cta button responsiveness

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,11 +16,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white text-slate-950 relative pt-28 sm:pt-36`}>
-      <div className="fixed top-0 left-0 h-full w-full z-[-10] bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(180,65%,55%,0)_0,rgba(130,175,255,0.2)_100%)]">    
-      <Header />
-      {children}   
+      <body className={`${inter.className} text-slate-950`}>
+        <div className="relative pt-14 sm:pt-18 top-0 left-0 h-full w-full bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(180,65%,55%,0)_0,rgba(130,175,255,0.3)_100%)]">
+          <Header />
+          {children}   
         </div>
+
       </body>
     
     </html>
